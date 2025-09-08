370131af39710e507edaa4e72b54e5afe6b88d0a
tobuubot <tobuubot@icloud.com>
2025-09-08 17:13:16 +0900

    Initial commit: React + Vite + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex min-h-screen bg-red-500">
+      <h1 className="text-5xl font-bold text-black">Hello Tailwind v4 🚀</h1>
+    </div>
+  );
+};
+
+export default App;
