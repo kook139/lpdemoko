a7fea65646a754819e09f4f3a953dbc4327607fc
tobuubot <tobuubot@icloud.com>
2025-09-08 17:34:56 +0900

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
     <div className="flex min-h-screen bg-red-500">
-      <h1 className="text-5xl font-bold text-black">Hello Tailwind v4 🚀</h1>
+      <h1 className="text-5xl font-bold text-black">
+        Hello Tailwind v4 YES 🚀
+      </h1>
     </div>
   );
 };
